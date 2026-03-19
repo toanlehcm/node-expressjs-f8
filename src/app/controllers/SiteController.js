b329ef503c63151b0a7e7d1b4995c120adfe83d6
Toan Le <thanhtoanhcm1602@gmail.com>
2026-03-20 04:05:21 +0700

    - L24: Update CourseShema. - Connect controller, models, mongodb compass.

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,8 +1,23 @@
+const Course = require('../models/Course');
+
 class SiteController {
     // function constructor.
     // GET /
-    index(req, res) {
-        res.render('home');
+    // index(req, res) {
+    //     res.render('home');
+    // }
+
+    async index(req, res) {
+        try {
+            // Using await to "wait" mongoose++ find data.
+            const courses = await Course.find({});
+            console.log('Courses:', courses);
+            res.json(courses);
+        } catch (err) {
+            // If there is an error, it will jump to this catch block.
+            console.log('Error fetching courses:', err);
+            res.status(400).json({ error: 'Failed to fetch courses' });
+        }
     }
 
     // GET /search
