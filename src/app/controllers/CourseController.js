d2f0bb60d07ae043de0c46c252d19e34503bc8f5
Toan Le <thanhtoanhcm1602@gmail.com>
2026-03-21 05:37:27 +0700

    L28. [CRUD] Create new course.

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -2,10 +2,26 @@ const Course = require('../models/Course');
 const { mongooseToObject } = require('../../util/mongoose');
 
 class CourseController {
+    // [GET] /courses/:slug
     show(req, res) {
         Course.findOne({ slug: req.params.slug })
             .then(course => res.render('courses/show', { course: mongooseToObject(course) }))
             .catch(err => res.status(400).json({ error: 'Failed to fetch course' }));
+    }
+
+    // [GET] /courses/create
+    create(req, res) {
+        res.render('courses/create');
+    }
+
+    // [POST] /courses/store
+    store(req, res) {
+        const formData = req.body;
+        formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+        const course = new Course(formData);
+        course.save()
+            .then(() => res.redirect('/'))
+            .catch(err => res.status(400).json({ error: 'Failed to create course' }));
     }
 }
 
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
+const mongooseSlug = require('mongoose-slug-updater');
 const Schema = mongoose.Schema;
 
+mongoose.plugin(mongooseSlug);
+
 const CourseSchema = new Schema({
-    name: { type: String, maxLength: 255 },
+    name: { type: String, maxLength: 255, required: true },
     description: { type: String, maxLength: 600 },
     image: { type: String, maxLength: 255 },
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
-});
+    videoId: { type: String, maxLength: 255, required: true },
+    level: { type: String, maxLength: 255 },
+    slug: { type: String, unique: true, slug: 'name' },
+    // createAt: { type: Date, default: Date.now },
+    // updateAt: { type: Date, default: Date.now },
+}, { timestamps: true });
 
 module.exports = mongoose.model('Course', CourseSchema);
